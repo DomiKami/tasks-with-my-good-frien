fc6e5b898ec15282f1b18c42f1afdc97a8c016b4
Domi Kami <35378527+DomiKami@users.noreply.github.com>
2020-09-28 23:45:31 +0200

    Add files via upload

---
+++ Dziobex1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string x;
+	
+	cout << "Podaj slowo" << endl;
+	getline(cin,x);
+	
+	for(int i=x.length()-1;i>=0;i--)
+		cout << x[i];
+		
+		
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// im big dumb
