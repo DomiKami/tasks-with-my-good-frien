3267a632a2657155ea138d8cdab096294f21656c
Domi Kami <35378527+DomiKami@users.noreply.github.com>
2020-10-10 22:40:12 +0200

    Add files via upload

---
+++ Dziobex4.1.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+using namespace std; 
+
+void FibbonacciFormula(int n) {
+    int n1 = 0, n2 = 1;
+    cout << n1 << ' ';
+    for (int i = 0; i < n; i++) {
+        cout << n2 << ' ';
+        n2 = n1 + n2;
+        n1 = n2 - n1; 
+    }
+}
+int main()
+{
+    int n;
+    do {
+        cout << "Give a num between <1;40>" << endl;
+        cin >> n;
+    } while (n <= 1 or n >= 40);
+
+     FibbonacciFormula(n);
+        return 0;
+}
+//  /                  \              _-_  _-_
+// /    ===   |   ===   \            /   \/   \
+///      0     )   0     \      --=  \  love  /
+//\           -          /\    /      \  ya  /
+// \       -_____-      /  \  /        \bro!/
+//  \                  /    \/          \  /    
