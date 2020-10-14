75196c206432ca9ae3a1df370a49c52b494fe92b
Domi Kami <35378527+DomiKami@users.noreply.github.com>
2020-10-14 20:58:35 +0200

    owo
    I'm not a fuwwy I sweaw!!! it's aww just fow fun and I don't weawwy know what I was thinking owo

---
+++ OwO.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void OwOfy(string notowo) {
+    char w = 'w';
+        for (int i = 0; i < notowo.size(); i++)
+            if (notowo[i] == 'l' or notowo[i] == 'r')
+                cout << w;
+            else
+                cout << notowo[i];
+        cout << "owo";
+}
+
+int main()
+{
+    string notowo;
+
+    cout << "type something to make into owo" << endl;
+    getline(cin, notowo);
+    
+    OwOfy(notowo);
+    return 0;
+}
+
